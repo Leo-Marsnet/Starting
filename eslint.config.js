3d3b25cf1d43a9da6a4c78d55d3e874b22770427
Leo-Marsnet <your-email@example.com>
2025-07-01 11:24:11 +0800

    chore: temporarily disable type-check to commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,35 @@
 import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import globals from "globals";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import react from "eslint-plugin-react";
+import reactCompiler from "eslint-plugin-react-compiler";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  { ignores: ["dist"] },
+  { ignores: ["dist", "*.cjs"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      react,
+      "jsx-a11y": jsxA11y,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
+      ...react.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "react-compiler/react-compiler": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
@@ -34,4 +45,5 @@ export default tseslint.config([
       "no-console": "warn",
     },
   },
+  prettier,
 ]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 /// <reference types="vitest" />
 
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vitest/config"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/postcss";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,4 +23,4 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/tests/setup.ts",
   },
-})
+});
